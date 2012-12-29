235426c17fb3bd1947f44aea3554443cd7795399
Ethan Blackburn <eblackb1@slu.edu>
2012-12-29 07:55:32 -0600

    Partition Problem solution
    This is a solution to the partition problem that implements the greedy
    algorithm

---
+++ PartitionProblem.py
@@ -0,0 +1,51 @@
+import random
+import math
+
+numbers = []
+left = []
+right = []
+balance = 0 #negative value if left sum is higher, positive value if right sum is higher and 0 if sums are equal
+
+def furthestVal(num):
+	largest_delta = 0 
+	large_key = 0
+	for key in numbers:
+		if(math.fabs(num-key) > largest_delta):
+			largest_delta = math.fabs(num-key)
+			large_key = key
+			
+	return large_key
+
+
+for i in range(1000):
+	numbers.append(random.randrange(1,1000,1))
+
+for j in range(1000):
+	if(balance != 0):
+		if(balance < 0):
+			far_val = furthestVal(balance)
+			right.append(far_val)
+			numbers.remove(far_val)
+			balance += far_val
+		else:
+			far_val = furthestVal(balance)
+			left.append(far_val)
+			numbers.remove(far_val)
+			balance -= far_val
+	else:
+		far_val = furthestVal(balance)
+		left.append(far_val)
+		numbers.remove(far_val)
+		balance -= far_val
+			
+print 'left: '	
+print left
+print 'right: '
+print right
+
+print 'balance: '
+print balance
+		
+
+
+	
